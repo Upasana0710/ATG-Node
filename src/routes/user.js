4d744fa0e2b11612410fb40a5cc67e335274aff3
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-07-05 10:43:01 +0530

    added routes for reset

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { signup, signin } from '../controllers/user.js';
+import { signup, signin, forgotPassword, verifyReset, resetPassword } from '../controllers/user.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/signin', signin);
+router.post('/forgotPassword', forgotPassword);
+router.get('/verifyReset', verifyReset);
+router.patch('/resetPassword', resetPassword);
 
 export default router;
