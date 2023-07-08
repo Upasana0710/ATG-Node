5ad5f594b9eebb886979eca20511b0ccb638cd99
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-07-09 01:44:35 +0530

    added post routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './src/routes/user.js';
+import postRoutes from './src/routes/post.js';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 
 app.use('/user', userRoutes);
+app.use('/post', postRoutes);
 
 const PORT = process.env.PORT || 5000;
 
