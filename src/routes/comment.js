c6407a03acdbebfa28b9851cb93563a0fca61346
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-07-09 14:42:23 +0530

    made update and delete api for comment

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createComment, getComments, createReply } from '../controllers/comment.js';
+import { createComment, getComments, createReply, deleteComment, updateComment } from '../controllers/comment.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.post('/:id', [verifyToken], createComment);
 router.get('/:id', getComments);
 router.post('/reply/:id', [verifyToken], createReply);
+router.patch('/:id', [verifyToken], updateComment);
+router.delete('/', [verifyToken], deleteComment)
 
 export default router;
