2f23e94fb58b165972e6664d1aa5c7872c958d3b
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-07-13 17:02:25 +0530

    updated user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { signup, signin, forgotPassword, verifyReset, resetPassword, getUserById } from '../controllers/user.js';
+import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
@@ -8,6 +9,6 @@ router.post('/signin', signin);
 router.post('/forgotPassword', forgotPassword);
 router.get('/verifyReset', verifyReset);
 router.patch('/resetPassword', resetPassword);
-router.get('/:id', getUserById);
+router.get('/:id', [verifyToken], getUserById);
 
 export default router;
